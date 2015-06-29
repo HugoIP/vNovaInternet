257e1cdc8621b1b5fd5f285a3b3a6e7eedf17b28
unknown <bairon.myh@gmail.com>
2015-06-29 01:34:16 -0500

    Frontent controller otimizado, TODO: controller para 404

--- helpers.php
+++ helpers.php
@@ -6,4 +6,19 @@ function view($template,$vars = array())
 	extract($vars);
 	require "views/$template.tpl.php";
 }
+function controller($name)
+{
+	$file = "controllers/$name.php";
+	//exit($file);   //Breakpoint
+	if(file_exists($file))
+	{
+		require $file;
+	}
+	else
+	{
+		//TODO: Create a controller for this case
+		header("HTML/1.0 404 Not Found");
+		exit("Pagina no encontrada");
+	}
+}
 ?>
--- index.php
+++ index.php
@@ -6,20 +6,9 @@ configurar nuestra aplicacion
 require'config.php';
 require'helpers.php';
 //llamar a los controladores individuales
-//index.php
 if(empty($_GET['url']))
 {
-	require "controllers/home.php";
+	$_GET['url']="home";
 }
-elseif($_GET['url'] == 'contactos')
-{
-	require "controllers/contactos.php";
-}
-else
-{
-	header("HTTP/1.0 404 Not Found");
-	exit("Pagina no encontrada");
-}
-//index.php?url=contactos
-//var_dump($_GET);
+controller($_GET['url']);
 ?>
