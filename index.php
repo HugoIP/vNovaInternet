54ef3a4bb812fb01adc80bdb4a9f2628696dc371
HugoIP <bairon.myh@gmail.com>
2015-06-29 01:46:06 -0500

    Version ready to upload to github

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ configurar nuestra aplicacion
 */
 require'config.php';
 require'helpers.php';
-//llamar a los controladores individuales
+//llamar al controlador de acuerdo a la url
 if(empty($_GET['url']))
 {
 	$_GET['url']="home";
